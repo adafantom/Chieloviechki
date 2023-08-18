f5363ecfcf98a3100c9f18ab2a4e793aa8a42a57
adafantom <61115044+adafantom@users.noreply.github.com>
2023-08-18 22:41:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import file_operations
+from faker import Faker
+import random
+
+fake = Faker("ru_RU")
+
+skills = ["Стремительный прыжок", "Электрический выстрел", "Ледяной удар", "Стремительный удар", "Кислотный взгляд", "Тайный побег", "Ледяной выстрел", "Огненный заряд"]
+
+alf = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+skills_pers = random.sample(skills, 3)
+
+for i in range(3):
+    skill = skills_pers[i]
+    for j in range(len(skill)):
+        skills_pers[i] = skills_pers[i].replace(skill[j],alf[skill[j]])
+
+context = {
+    "first_name": fake.first_name(),
+    "last_name": fake.last_name(),
+    "job": fake.job(),
+    "town": fake.city(),
+    "strength": random.randint(3, 18),
+    "agility": random.randint(3, 18),
+    "endurance": random.randint(3, 18),
+    "intelligence": random.randint(3, 18),
+    "luck": random.randint(3, 18),
+    "skill_1": skills_pers[0],
+    "skill_2": skills_pers[1],
+    "skill_3": skills_pers[2]
+}
+
+for q in range(10):
+    file_name = 'files/file{}.txt'.format(q+1)
+    file_operations.render_template("template.txt", file_name, context)
